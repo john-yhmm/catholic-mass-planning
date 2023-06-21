0e46a21382ad677196c7bfb5e29181aab6faff00
John <johnyehtetmgmg@gmail.com>
2023-06-21 22:36:14 +0630

    [Back-End] Initialize Project

---
+++ backend/src/test/java/com/jyhmm/cmp/CmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyhmm.cmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
